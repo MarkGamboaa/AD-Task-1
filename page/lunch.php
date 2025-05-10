fe0cb598f95407ade3787c6da58f0ecdd02361a0
MarkGamboa <202310110@fit.edu.ph>
2025-05-10 16:15:59 +0800

    fix (Content Update): Correct title and heading for lunch page

--- page/lunch.php
+++ page/lunch.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dinner</title>
+    <title>Lunch</title>
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
-    <h1>Dinner Meals</h1>
+    <h1>Lunch Meals</h1>
 </body>
 </html>
