1f1f18b4e298669f7d0913389ef43c72516976f5
MarkGamboa <202310110@fit.edu.ph>
2025-05-10 15:58:26 +0800

    feat (Content Addition): Add main heading and navigation links to the meal planner

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
+    <h1>Weekly Meal Planner</h1>
+    <div class="links">
+        <a href="page/breakfast.php">Breakfast</a>
+        <a href="page/lunch.php">Lunch</a>
+        <a href="page/dinner.php">Dinner</a>
+    </div>
 </body>
 </html>
