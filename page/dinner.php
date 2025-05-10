68521daf6a05a9c43fbc61ef936a73ecec158ecb
MarkGamboa <202310110@fit.edu.ph>
2025-05-10 16:29:37 +0800

    feat (Content Addition) : Add conditional statement to output dinner meals

--- page/dinner.php
+++ page/dinner.php
@@ -12,7 +12,14 @@
         <?php
         $days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
         $meals = ["Sandwich", "Rice & Chicken", "Pasta", "Burger", "Steak", "Pizza", "Sushi"];
-        
+
+        for ($i = 0; $i < count($days); $i++) {
+            if ($days[$i] === "Saturday" || $days[$i] === "Sunday") {
+              echo "<li style='color:red; font-weight:bold'>{$days[$i]}: {$meals[$i]}</li>";
+            } else {
+              echo "<li style='color:green;'>{$days[$i]}: {$meals[$i]}</li>";
+            }
+          }
         ?>
     </ul>
 </body>
